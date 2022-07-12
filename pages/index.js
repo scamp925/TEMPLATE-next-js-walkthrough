7c467289eff250f043d6bb12fea05e553bc8c391
Sariah Campopiano <scampopiano925@gmail.com>
2022-07-11 19:25:24 -0500

    Using .map to dynamically render counterTitle

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
 import Counter from '../components/Counter';
 
+const counters = [
+  { title: 'Counter 1' },
+  { title: 'Counter 2' },
+  { title: 'Counter 3' },
+  { title: 'Counter 4' },
+  { title: 'Counter 5' },
+];
+
 function Home() {
   return (
     <>
-      <Counter counterTitle="Counter 1" />
-      <Counter counterTitle="Counter 2" />
+      {counters.map(({ title }) => (
+        <Counter counterTitle={title} />
+      ))};
     </>
   );
 }
